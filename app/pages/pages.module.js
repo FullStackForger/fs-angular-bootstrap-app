35fa3d45b43f91ea841491e2098e67340ab32b31
IndieForger <indieforger@gmail.com>
2015-10-02 17:59:10 +0100

    pages.module.js

--- app/pages/pages.module.js
+++ app/pages/pages.module.js
@@ -1,1 +1,6 @@
-angular.module('IFSP.App.Pages', [])
+angular.module('IFSP.App.Pages', [
+    'IFSP.App.Pages.Home',
+    'IFSP.App.Pages.Info',
+    'IFSP.App.Pages.Profile',
+    'IFSP.App.Pages.Settings'
+])

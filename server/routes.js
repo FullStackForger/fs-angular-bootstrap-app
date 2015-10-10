d394c8a19b5e8bcfb5544809103861de60b9e9b1
IndieForger <indieforger@gmail.com>
2015-10-10 17:03:55 +0100

    node: signup route

--- server/routes.js
+++ server/routes.js
@@ -1,18 +1,40 @@
 'use strict'
-const express = require('express')
-const router = express.Router()
+const Express = require('express')
+const Router = Express.Router()
+const Model = require('./model')
+const User = Model.User;
 
-router.get('/status', function (req, res) {
+Router.get('/status', function (req, res) {
   res.send({
 		status: 'ok'
 	})
 })
 
-router.get('/me', function (req, res) {
+Router.get('/me', function (req, res) {
  res.send({
 		username: 'test user',
 		profile: 'my profile lorem ipsum and some other pipsum'
 	})
 })
 
-module.exports = router
+Router.post('/auth/signup', function(req, res) {
+  User.findOne({ email: req.body.email }, function(err, existingUser) {
+    if (existingUser) {
+      return res.status(409).send({ message: 'Email is already taken' });
+    }
+    var user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    });
+    user.save(function(err, result) {
+      if (err) {
+        res.status(500).send({ message: err.message });
+      }
+      //res.send({ token: createJWT(user) })
+      res.send({ token: 'asdzxcasd123weqweasd' })
+    });
+  });
+});
+
+module.exports = Router

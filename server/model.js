128258b55196f9c69d5ccff31fe978b17c15307e
IndieForger <indieforger@gmail.com>
2015-10-14 19:26:48 +0100

    User model updated

--- server/model.js
+++ server/model.js
@@ -1,11 +1,11 @@
 'use strict'
 const Bcrypt = require('bcryptjs')
-const Mongoose = require('Mongoose')
+const Mongoose = require('mongoose')
 
 const userSchema = new Mongoose.Schema({
   email: { type: String, unique: true, lowercase: true },
   password: { type: String, select: false },
-  username: String
+  username: { type: String }
 })
 
 userSchema.pre('save', function(next) {
@@ -14,7 +14,13 @@ userSchema.pre('save', function(next) {
     return next()
   }
   Bcrypt.genSalt(10, function(err, salt) {
+		if (err) {
+			return next(err)
+		}
     Bcrypt.hash(user.password, salt, function(err, hash) {
+			if (err) {
+				return next(err)
+			}
       user.password = hash
       next()
     })
@@ -30,7 +36,7 @@ userSchema.methods.comparePassword = function(password, done) {
 const User = Mongoose.model('User', userSchema)
 
 function connect() {
-	Mongoose.connect('localhost')
+	Mongoose.connect('mongodb://localhost/ifsp_app');
 	Mongoose.connection.on('error', function(err) {
 		console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red)
 	})

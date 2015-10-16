e9beed1879be725def85f501829f670c3917ac01
IndieForger <indieforger@gmail.com>
2015-10-16 19:32:33 +0100

    connecting to datasbse

--- server/server.js
+++ server/server.js
@@ -5,17 +5,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
 const app = express()
-
 const routes = require('./routes')
+
+const Model = require('./model')
+
+Model.connect();
 
 app.set('port', process.env.PORT || 8080)
 app.use(cors())
 app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../app')))
-
 app.use('/api', routes)
 
 app.listen(app.get('port'), function() {

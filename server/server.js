1f9e733d713f354e6c2fd2b6878ba2f1f2aa0795
IndieForger <indieforger@gmail.com>
2015-10-17 22:34:12 +0100

    comments and cleanup

--- server/server.js
+++ server/server.js
@@ -5,20 +5,26 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
 const app = express()
-const routes = require('./routes')
 
+// local dependencies
+const routes = require('./routes')
 const Model = require('./model')
 
+// connect to DB
 Model.connect();
 
+// express middleware
 app.set('port', process.env.PORT || 8080)
 app.use(cors())
 app.use(logger('dev'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '../app')))
+
+// routes
 app.use('/api', routes)
 
+// start server
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'))
 })

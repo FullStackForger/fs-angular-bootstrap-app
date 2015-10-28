ec8a6fcb78a5172ab6f2a499e698d7c4495132ab
IndieForger <indieforger@gmail.com>
2015-10-28 20:28:14 +0000

    login ctrl scaffold

---
+++ app/pages/login/login.controller.js
@@ -0,0 +1,18 @@
+(function() {
+'use strict';
+
+	angular
+		.module('IFSP.App.Pages.Login'')
+		.controller('LoginController', LoginController);
+
+	LoginController.$inject = [''];
+	function LoginController() {
+		var vm = this;
+
+		vm.login = function() {
+				// login user
+				console.log('logging in');
+		}
+
+	}
+})();

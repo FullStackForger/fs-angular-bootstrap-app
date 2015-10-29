39444081ccbd7b734fe8aeda29aadafec0904bc8
IndieForger <indieforger@gmail.com>
2015-10-29 20:54:27 +0000

    missing page dependecies

--- app/pages/pages.module.js
+++ app/pages/pages.module.js
@@ -1,6 +1,9 @@
 angular.module('IFSP.App.Pages', [
 	'IFSP.App.Pages.Home',
 	'IFSP.App.Pages.Info',
+	'IFSP.App.Pages.Login',
+	'IFSP.App.Pages.Logout',
 	'IFSP.App.Pages.Profile',
+	'IFSP.App.Pages.Signup',
 	'IFSP.App.Pages.Settings'
 ])

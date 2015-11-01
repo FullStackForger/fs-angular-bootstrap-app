79b8b413b27f4033a9143a3f6ce59eba26ed67e7
IndieForger <indieforger@gmail.com>
2015-11-01 21:32:44 +0000

    info route update

--- app/pages/info/info.module.js
+++ app/pages/info/info.module.js
@@ -1,1 +1,9 @@
-angular.module('IFSP.App.Pages.Info', [])
+(function () {
+	angular
+		.module('IFSP.App.Pages.Info', ['ngRoute'])
+		.config(['$routeProvider', function($routeProvider) {
+			$routeProvider.when('/info', {
+				templateUrl: 'pages/info/info.tpl.html',
+			})
+		}])
+})()

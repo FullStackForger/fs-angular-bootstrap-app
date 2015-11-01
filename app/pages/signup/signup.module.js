44ec751370c4666ce34cb0609312ce5fd6182844
IndieForger <indieforger@gmail.com>
2015-11-01 22:39:16 +0000

    signup module dependencies

--- app/pages/signup/signup.module.js
+++ app/pages/signup/signup.module.js
@@ -3,6 +3,8 @@
 		.module('IFSP.App.Pages.Signup', ['ngRoute'])
 		.config(['$routeProvider', function($routeProvider) {
 			$routeProvider.when('/signup', {
+				controller: 'SignupController',
+				controllerAs: 'signupCtrl',
 				templateUrl: 'pages/signup/signup.tpl.html',
 			})
 		}])

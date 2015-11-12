7f29d045265b8a01162f1401f330d80f133d3f8a
IndieForger <indieforger@gmail.com>
2015-11-12 21:07:04 +0000

    login module spec extended

--- app/pages/login/login.module.spec.js
+++ app/pages/login/login.module.spec.js
@@ -8,9 +8,28 @@ describe('IFSP.App.Pages.Login', function() {
 
 	beforeEach(module('IFSP.App.Pages.Login'));
 
-	it('should load the template', inject(function($route) {
-		expect($route.routes[moduleRoute].controller).toEqual(controllerStr);
-		expect($route.routes[moduleRoute].templateUrl).toEqual(templateUrl);
-	}));
+	describe('auto routing', function() {
+		it('should load the template', inject(function($route) {
+			expect($route.routes[moduleRoute].controller).toEqual(controllerStr);
+			expect($route.routes[moduleRoute].templateUrl).toEqual(templateUrl);
+		}));
+	});
 
+	describe('location change', function() {
+		it('should load the template', inject(function($route, $location, $rootScope) {
+
+			var $httpBackend;
+			inject(function($injector) {
+				$httpBackend = $injector.get('$httpBackend');
+			});
+			$httpBackend.whenGET(templateUrl).respond('<div></div>');
+
+			expect($route.current).toBeUndefined();
+			$location.path(moduleRoute);
+			$rootScope.$digest();
+
+			expect($route.current.templateUrl).toEqual(templateUrl);
+			expect($route.current.controller).toEqual(controllerStr);
+		}));
+	});
 });

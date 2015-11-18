b13b309d54b515526f53042c34ea563384cca939
IndieForger <indieforger@gmail.com>
2015-11-18 18:09:46 +0000

    cleanup

--- app/pages/pages.module.js
+++ app/pages/pages.module.js
@@ -1,8 +1,4 @@
 angular.module('IFSP.App.Pages', [
-	// global dependecies
-	'ngAnimate',
-	'toastr',
-	// local dependecies
 	'IFSP.App.Pages.Home',
 	'IFSP.App.Pages.Info',
 	'IFSP.App.Pages.Login',

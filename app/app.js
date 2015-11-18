eea3ab32c3b5ad076596ac1813fb2a2d773275a3
IndieForger <indieforger@gmail.com>
2015-11-18 18:00:01 +0000

    satellizer configuration

--- app/app.js
+++ app/app.js
@@ -1,10 +1,32 @@
-angular
-    .module('IFSP.App', [
-        'ngRoute',
-				'IFSP.App.Pages'
-    ])
-    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-        $routeProvider.otherwise({
-            redirectTo: '/home'
-        });
-    }])
+(function() {
+	'use strict'
+
+	var app = 	angular.module('IFSP.App', [
+			// global dependecies
+			'ngRoute',
+			'ngAnimate',
+			// 3d party depedencies
+			'toastr',
+			'satellizer',
+			// local depdenencies
+			'IFSP.App.Pages'
+	])
+
+	app.config(['$routeProvider', '$locationProvider', '$authProvider',
+		function ($routeProvider, $locationProvider, $authProvider) {
+
+		$routeProvider.otherwise({
+				redirectTo: '/home'
+		})
+
+		$authProvider.baseUrl = '/'
+		$authProvider.loginUrl = '/api/auth/login'
+		$authProvider.signupUrl = '/api/auth/signup'
+		$authProvider.unlinkUrl = '/api/auth/unlink/'
+
+		$authProvider.facebook({
+			clientId: '771579649652176'
+		})
+	}])
+
+})()

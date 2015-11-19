0e478925f0d80d5178c2cf34ddc796d86db69b5a
IndieForger <indieforger@gmail.com>
2015-11-19 19:07:38 +0000

    logout module

--- app/pages/logout/logout.module.js
+++ app/pages/logout/logout.module.js
@@ -1,6 +1,14 @@
-(function() {
+(function () {
 	'use strict';
-	angular.module('IFSP.App.Pages.Logout', [
-   'ngRoute'
+	var module = angular.module('IFSP.App.Pages.Logout', [
+		'ngRoute'
 	])
+
+	module.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+			.when('/logout', {
+				controller: 'LogoutController as logoutCtrl',
+				templateUrl: 'pages/logout/logout.tpl.html'
+			})
+	}])
 })()

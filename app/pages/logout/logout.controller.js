78a6f0d37b20f9a51845ca6814ae70ef7eb720ce
IndieForger <indieforger@gmail.com>
2015-11-21 19:29:18 +0000

    logout ctrl

--- app/pages/logout/logout.controller.js
+++ app/pages/logout/logout.controller.js
@@ -1,15 +1,33 @@
 (function() {
-'use strict';
-
+'use strict'
 	angular
 		.module('IFSP.App.Pages.Logout')
-		.controller('LogoutController', LogoutController);
+		.controller('LogoutController', LogoutController)
 
-	LogoutController.$inject = [];
-	function LogoutController() {
-		var vm = this;
+	LogoutController.$inject = ['$location', '$auth', '$interval', 'toastr']
+	function LogoutController($location, $auth, $interval, toastr) {
+		var vm = this
+		vm.timer = null
 
 		// auto - log out
 		console.log('logging out')
+
+		$auth
+			.logout()
+			.then(function() {
+				toastr.info('You have been logged out')
+				initRedirect(5)
+			});
+
+		function initRedirect(delay) {
+			vm.timer = delay
+			$interval(function() {
+					vm.timer -= 1
+					if (vm.timer <= 0) {
+						$interval.cancel()
+						$location.path('/')
+					}
+				}, 1000)
+		}
 	}
 })();

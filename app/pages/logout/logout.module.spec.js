a1c0f3190e7b63eda964ed294073882f87756e42
IndieForger <indieforger@gmail.com>
2015-11-22 16:41:29 +0000

    logout module spec

---
+++ app/pages/logout/logout.module.spec.js
@@ -0,0 +1,35 @@
+'use strict'
+describe('IFSP.App.Pages.Logout', function() {
+
+	var
+		moduleRoute = '/logout',
+		templateUrl = 'pages/logout/logout.tpl.html',
+		controllerStr = 'LogoutController as logoutCtrl'
+
+	beforeEach(module('IFSP.App.Pages.Logout'))
+
+	describe('auto routing', function() {
+		it('should load the template', inject(function($route) {
+			expect($route.routes[moduleRoute].controller).toEqual(controllerStr)
+			expect($route.routes[moduleRoute].templateUrl).toEqual(templateUrl)
+		}));
+	});
+
+	describe('location change', function() {
+		it('should load the template', inject(function($route, $location, $rootScope) {
+
+			var $httpBackend
+			inject(function($injector) {
+				$httpBackend = $injector.get('$httpBackend')
+			})
+			$httpBackend.whenGET(templateUrl).respond('<div></div>')
+
+			expect($route.current).toBeUndefined();
+			$location.path(moduleRoute);
+			$rootScope.$digest();
+
+			expect($route.current.templateUrl).toEqual(templateUrl)
+			expect($route.current.controller).toEqual(controllerStr)
+		}));
+	});
+});

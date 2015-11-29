4f4d40070902aa9cf7fc3441e42058fd827066d7
IndieForger <indieforger@gmail.com>
2015-11-29 12:51:25 +0000

    ctrl as syntax update

--- app/pages/signup/signup.module.js
+++ app/pages/signup/signup.module.js
@@ -1,10 +1,10 @@
 (function () {
+	'use strict'
 	angular
 		.module('IFSP.App.Pages.Signup', ['ngRoute'])
 		.config(['$routeProvider', function($routeProvider) {
 			$routeProvider.when('/signup', {
-				controller: 'SignupController',
-				controllerAs: 'signupCtrl',
+				controller: 'SignupController as signupCtrl',
 				templateUrl: 'pages/signup/signup.tpl.html',
 			})
 		}])

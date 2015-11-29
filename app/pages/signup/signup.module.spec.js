51652579608e98738f2e32dafbcc5d321b2cf1ef
IndieForger <indieforger@gmail.com>
2015-11-29 12:52:05 +0000

    signum module spec

---
+++ app/pages/signup/signup.module.spec.js
@@ -0,0 +1,34 @@
+describe('IFSP.App.Pages.Signup', function() {
+	'use strict'
+	var
+		moduleRoute = '/signup',
+		templateUrl = 'pages/signup/signup.tpl.html',
+		controllerStr = 'SignupController as signupCtrl'
+
+	beforeEach(module('IFSP.App.Pages.Signup'))
+
+	describe('auto routing', function() {
+		it('should load the template', inject(function($route) {
+			expect($route.routes[moduleRoute].controller).toEqual(controllerStr)
+			expect($route.routes[moduleRoute].templateUrl).toEqual(templateUrl)
+		}));
+	});
+
+	describe('location change', function() {
+		it('should load the template', inject(function($route, $location, $rootScope) {
+
+			var $httpBackend
+			inject(function($injector) {
+				$httpBackend = $injector.get('$httpBackend')
+			})
+			$httpBackend.whenGET(templateUrl).respond('<div></div>')
+
+			expect($route.current).toBeUndefined();
+			$location.path(moduleRoute);
+			$rootScope.$digest();
+
+			expect($route.current.templateUrl).toEqual(templateUrl)
+			expect($route.current.controller).toEqual(controllerStr)
+		}));
+	});
+});

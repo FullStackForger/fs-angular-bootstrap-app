ab2d449ecff7004e78ac96e30babd12a12c25867
IndieForger <indieforger@gmail.com>
2015-11-30 15:44:16 +0000

    settings route secured

--- app/pages/settings/settings.module.js
+++ app/pages/settings/settings.module.js
@@ -2,8 +2,23 @@
 	angular
 		.module('IFSP.App.Pages.Settings', ['ngRoute'])
 		.config(['$routeProvider', function($routeProvider) {
+
 			$routeProvider.when('/settings', {
 				templateUrl: 'pages/settings/settings.tpl.html',
+				resolve: {
+					restrictToRegistered: restrictToRegistered
+				}
 			})
+
+			function restrictToRegistered ($q, $location, $auth) {
+				var deferred = $q.defer()
+				if ($auth.isAuthenticated()) {
+					deferred.resolve()
+				} else {
+					$location.path('/login')
+					deferred.reject()
+				}
+				return deferred.promise
+			}
 		}])
 })()

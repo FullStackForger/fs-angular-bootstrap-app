010b6242b33f94c1d07894e85b62292f6fca3c8c
IndieForger <indieforger@gmail.com>
2015-11-30 18:16:16 +0000

    redirection to login from profile

--- app/pages/profile/profile.module.js
+++ app/pages/profile/profile.module.js
@@ -1,9 +1,24 @@
 (function () {
 	angular
 		.module('IFSP.App.Pages.Profile', ['ngRoute'])
-		.config(['$routeProvider', function($routeProvider) {
+		.config(['$routeProvider', function ($routeProvider) {
+
 			$routeProvider.when('/profile', {
 				templateUrl: 'pages/profile/profile.tpl.html',
+				resolve: {
+					loginRequired: loginRequired
+				}
 			})
+
+			function loginRequired ($q, $location, $auth) {
+				var deferred = $q.defer();
+				if ($auth.isAuthenticated()) {
+					deferred.resolve();
+				} else {
+					$location.path('/login');
+					deferred.reject();
+				}
+				return deferred.promise;
+			};
 		}])
 })()

21a190ab8388d1dfaee46bfa9841e0ae4069ad9e
IndieForger <indieforger@gmail.com>
2015-12-01 14:23:41 +0000

    toastr popups added to route resolver

--- app/common/resolver.constant.js
+++ app/common/resolver.constant.js
@@ -8,21 +8,23 @@
 			allowGuestOnly: allowGuestOnly
 		})
 
-	function allowRegisteredOnly($q, $location, $auth) {
+	function allowRegisteredOnly($q, $location, $auth, toastr) {
 		var deferred = $q.defer()
 		if ($auth.isAuthenticated()) {
 			deferred.resolve()
 		} else {
+			toastr.warning('Log in to view this page.')
 			deferred.reject()
 			$location.path('/login')
 		}
 		return deferred.promise
 	}
 
-	function allowGuestOnly($q, $location, $auth) {
+	function allowGuestOnly($q, $location, $auth, toastr) {
 		var deferred = $q.defer()
 		console.log($location.path());
 		if ($auth.isAuthenticated()) {
+			toastr.warning('You are already logged in.')
 			deferred.reject()
 			$location.path('/')
 		} else {

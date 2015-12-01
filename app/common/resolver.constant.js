23ef17da7ca9489a789f5c9f57dd0108d748b868
IndieForger <indieforger@gmail.com>
2015-12-01 14:04:44 +0000

    route resolver extended

--- app/common/resolver.constant.js
+++ app/common/resolver.constant.js
@@ -7,15 +7,25 @@
 			allowRegisteredOnly: allowRegisteredOnly
 		})
 
-		function allowRegisteredOnly ($q, $location, $auth) {
-				var deferred = $q.defer()
-				if ($auth.isAuthenticated()) {
-					deferred.resolve()
-				} else {
-					$location.path('/login')
-					deferred.reject()
-				}
-				return deferred.promise
-		}
+	function allowRegisteredOnly($q, $location, $auth) {
+			var deferred = $q.defer()
+			if ($auth.isAuthenticated()) {
+		deferred.resolve()
+			} else {
+		$location.path('/login')
+		deferred.reject()
+			}
+			return deferred.promise
+	}
 
-})();
+	function allowGuestOnly($q, $auth) {
+		var deferred = $q.defer();
+		if ($auth.isAuthenticated()) {
+			deferred.reject();
+		} else {
+			deferred.resolve();
+		}
+		return deferred.promise;
+	}
+
+})()

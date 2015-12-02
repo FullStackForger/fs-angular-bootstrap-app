13f66a010be60bc5d03edad11bd559fa0f204b37
IndieForger <indieforger@gmail.com>
2015-12-02 20:19:42 +0000

    code format

--- app/app.js
+++ app/app.js
@@ -1,31 +1,31 @@
-(function() {
+(function () {
 	'use strict'
-	var app = 	angular.module('IFSP.App', [
-			// global dependecies
-			'ngRoute',
-			'ngAnimate',
-			'satellizer',
-			'toastr',
-			// local depdenencies
-			'IFSP.App.Pages',
-			'IFSP.App.Common'
+	var app = angular.module('IFSP.App', [
+		// global dependecies
+		'ngRoute',
+		'ngAnimate',
+		'satellizer',
+		'toastr',
+		// local depdenencies
+		'IFSP.App.Pages',
+		'IFSP.App.Common'
 	])
 
 	app.config(['$routeProvider', '$locationProvider', '$authProvider',
 		function ($routeProvider, $locationProvider, $authProvider) {
 
-		$routeProvider.otherwise({
+			$routeProvider.otherwise({
 				redirectTo: '/home'
-		})
+			})
 
-		$authProvider.baseUrl = '/'
-		$authProvider.loginUrl = '/api/auth/login'
-		$authProvider.signupUrl = '/api/auth/signup'
-		$authProvider.unlinkUrl = '/api/auth/unlink/'
+			$authProvider.baseUrl = '/'
+			$authProvider.loginUrl = '/api/auth/login'
+			$authProvider.signupUrl = '/api/auth/signup'
+			$authProvider.unlinkUrl = '/api/auth/unlink'
 
-		$authProvider.facebook({
-			clientId: '771579649652176'
-		})
-	}])
+			$authProvider.facebook({
+				clientId: '771579649652176'
+			})
+		}])
 
 })()

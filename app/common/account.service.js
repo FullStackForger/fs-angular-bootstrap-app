8b7ad05be2352e650a5f6813d05dd771f7a72ed5
IndieForger <indieforger@gmail.com>
2015-12-03 18:41:44 +0000

    cleanup

--- app/common/account.service.js
+++ app/common/account.service.js
@@ -1,15 +1,15 @@
 (function () {
+	'use strict'
 	angular
 		.module('IFSP.App.Common')
 		.factory('account', function($http, $q) {
 			return {
 				getProfile: function() {
-					return $http.get('/api/me');
+					return $http.get('/api/me')
 				},
 				updateProfile: function(profileData) {
-					return $http.put('/api/me', profileData);
+					return $http.put('/api/me', profileData)
 				}
-			};
-		});
-
+			}
+		})
 })()

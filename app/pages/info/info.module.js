91293f5ff6b0ce6353fb7dd9eba070cd620214a9
IndieForger <indieforger@gmail.com>
2015-12-07 22:53:14 +0000

    info dependencies

--- app/pages/info/info.module.js
+++ app/pages/info/info.module.js
@@ -1,6 +1,9 @@
 (function () {
 	angular
-		.module('IFSP.App.Pages.Info', ['ngRoute'])
+		.module('IFSP.App.Pages.Info', [
+			'ngRoute',
+			'satellizer',
+		])
 		.config(['$routeProvider', function($routeProvider) {
 			$routeProvider.when('/info', {
 				templateUrl: 'pages/info/info.tpl.html',

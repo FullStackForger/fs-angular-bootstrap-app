5d0fef5cde4667ba1d8629f07abb5b585ac2e2de
IndieForger <indieforger@gmail.com>
2015-12-13 21:49:38 +0000

    client path updated

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '../app')))
+app.use(express.static(path.join(__dirname, '../client')))
 
 // routes
 app.use('/api', routes)

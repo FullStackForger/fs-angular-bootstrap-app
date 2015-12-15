08a50dd4142e7f24a0fdabd4d42ab3c96578ef2f
IndieForger <indieforger@gmail.com>
2015-12-15 04:17:18 +0000

    IIFE

--- client/app/pages/pages.module.js
+++ client/app/pages/pages.module.js
@@ -1,9 +1,11 @@
-angular.module('IFSP.App.Pages', [
-	'IFSP.App.Pages.Home',
-	'IFSP.App.Pages.Info',
-	'IFSP.App.Pages.Login',
-	'IFSP.App.Pages.Logout',
-	'IFSP.App.Pages.Profile',
-	'IFSP.App.Pages.Signup',
-	'IFSP.App.Pages.Settings'
-])
+(function () {
+	angular.module('IFSP.App.Pages', [
+		'IFSP.App.Pages.Home',
+		'IFSP.App.Pages.Info',
+		'IFSP.App.Pages.Login',
+		'IFSP.App.Pages.Logout',
+		'IFSP.App.Pages.Profile',
+		'IFSP.App.Pages.Signup',
+		'IFSP.App.Pages.Settings'
+	])
+})()

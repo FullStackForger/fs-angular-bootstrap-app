4aa91c987fbd0487e5368842f6b8a953937d6324
IndieForger <indieforger@gmail.com>
2015-12-24 16:18:32 +0000

    navbar ctrl injection removed

--- client/app/com/navbar.controller.js
+++ client/app/com/navbar.controller.js
@@ -1,15 +1,13 @@
-(function() {
-'use strict';
-
+(function () {
+	'use strict';
 	angular
 		.module('app.com')
 		.controller('NavbarController', NavbarController);
 
-	NavbarController.$inject = ['$auth'];
 	function NavbarController($auth) {
 			var vm = this;
 			vm.isAuthenticated = function() {
 				return $auth.isAuthenticated();
 			};
 	}
-})();
+})()
